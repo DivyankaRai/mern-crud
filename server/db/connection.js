acefd4771c12ee923a8cc181f67bc0cd344703ad
Divyanka Rai <raidivyanka40@gmail.com>
2023-01-26 11:35:39 +0530

    practice

--- server/db/connection.js
+++ server/db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 // const DB = process.env.DATABASE
 mongoose.set('strictQuery', false)
 
-mongoose.connect("mongodb://localhost:27017/merncrud",{
+mongoose.connect("mongodb+srv://divyanka:diviirai@mern.e7scgpt.mongodb.net/?retryWrites=true&w=majority",{
         useNewUrlParser: true
         // useUnifiedToplogy:true,
 }).then(() => {
